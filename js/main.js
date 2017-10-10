25d4f503b530d012b2ad4ee738786a0758acbbd7
Aaron <aaron@asouer.com>
2017-10-10 15:13:47 -0500

    changed more stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 function downloadModal() {
+    var modal = document.getElementById("download-modal");
+    modal.style.display = "block";
+}
+function mapModal() {
+    var modal = document.getElementById("map-modal");
+    modal.style.display = "block";
+}
 
+function closeModal() {
+    var map_modal = document.getElementById("map-modal");
+    map_modal.style.display = "none";
+    var download_modal = document.getElementById("download-modal");
+    download_modal.style.display = "none";
 }
