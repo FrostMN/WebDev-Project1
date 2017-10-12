50ce97a1ebaf592124c1ce376e373f5d95ffc92b
Aaron <aaron@asouer.com>
2017-10-12 17:30:37 -0500

    will present correct download based on os

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function downloadModal() {
     var modal = document.getElementById("download-modal");
     modal.style.display = "block";
-    detectOS();
+    setDownload();
 }
 function mapModal() {
     var modal = document.getElementById("map-modal");
@@ -15,11 +15,49 @@ function closeModal() {
     download_modal.style.display = "none";
 }
 
+function setDownload() {
+    var os = detectOS();
+    var modpack = document.getElementById("modpack");
+    var modload = document.getElementById("modload");
+    var modload_info = document.getElementById("modload-info");
+    var modpack_link = document.getElementById("modpack-link");
+    var modload_link = document.getElementById("modload-link");
+    var img_test = document.getElementById("modload-img");
+    if (os == "mac") {
+        if (img_test == null) {
+            var mac_img = document.createElement("img");
+            mac_img.setAttribute("src", "img/mac.png");
+            mac_img.setAttribute("id", "modload-img");
+            modload_link.appendChild(mac_img);
+            modload_link.setAttribute("href", "downloads/forge/forge-1.7.10-10.13.4.1558-1.7.10-installer.jar");
+            modload_info.innerHTML = "MacOS Mod Loader Download<br>" +
+                "<span id='lin2'>forge-1.7.10-10.13.4.1558-1.7.10-installer.jar</span>";
+        }
+    }
+
+    if (os == "win") {
+        var win_img = document.createElement("img");
+        win_img.setAttribute("src", "img/win.png");
+        win_img.setAttribute("id", "modload-img");
+        modload_link.appendChild(win_img);
+        modload_link.setAttribute("href", "downloads/forge/forge-1.7.10-10.13.4.1558-1.7.10-installer-win.exe");
+        modload_info.innerHTML = "Windows Mod Loader Download<br>" +
+            "<span id='lin2'>forge-1.7.10-10.13.4.1558-1.7.10-installer-win.exe</span>";
+    }
+
+    if (os == "lin") {
+        var lin_img = document.createElement("img");
+        lin_img.setAttribute("src", "img/lin.png");
+        lin_img.setAttribute("id", "modload-img");
+        modload_link.appendChild(lin_img);
+        modload_link.setAttribute("href", "downloads/forge/forge-1.7.10-10.13.4.1558-1.7.10-universal.jar");
+        modload_info.innerHTML = "Linux Mod Loader Download<br>" +
+            "<span id='lin2'>forge-1.7.10-10.13.4.1558-1.7.10-universal.jar</span>";
+    }
+}
+
 function detectOS() {
-    alert("in detect os");
-
-    var os = navigator.appVersion;
-
-
+    var os = navigator.appVersion.substr(5, 3).toLowerCase();
+    return os;
 }
 
