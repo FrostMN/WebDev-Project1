aa39ae0ddeeaada45707557a02cec545e933c05e
Aaron <aaron@asouer.com>
2017-10-13 07:59:51 -0500

    css tweaks

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 function downloadModal() {
-    var modal = document.getElementById("download-modal");
-    modal.style.display = "block";
     setDownload();
+    var modal = document.getElementById("download-modal");
+    var modal_win = document.getElementById("download-modal-content");
+    modal.style.display = "block";
+    fadeIn(modal_win);
 }
+
 function mapModal() {
     var modal = document.getElementById("map-modal");
+    var modal_win = document.getElementById("map-modal-content");
     modal.style.display = "block";
+    fadeIn(modal_win);
+}
+
+function openModal(name) {
+    setDownload();
+    var modal = document.getElementById(name);
+    var modal_win = document.getElementById(name + "-content");
+    modal.style.display = "block";
+    fadeIn(modal_win);
 }
 
 function closeModal() {
     var map_modal = document.getElementById("map-modal");
+    var map_win = document.getElementById("map-modal-content");
+    fadeOut(map_win);
     map_modal.style.display = "none";
     var download_modal = document.getElementById("download-modal");
+    var download_win = document.getElementById("download-modal-content");
+    fadeOut(download_win)
     download_modal.style.display = "none";
 }
 
 function setDownload() {
     var os = detectOS();
-
-    alert(os);
-
     var modpack = document.getElementById("modpack");
     var modload = document.getElementById("modload");
     var modload_info = document.getElementById("modload-info");
@@ -39,28 +53,62 @@ function setDownload() {
     }
 
     if (os == "win") {
-        var win_img = document.createElement("img");
-        win_img.setAttribute("src", "img/win.png");
-        win_img.setAttribute("id", "modload-img");
-        modload_link.appendChild(win_img);
-        modload_link.setAttribute("href", "downloads/forge/forge-1.7.10-10.13.4.1558-1.7.10-installer-win.exe");
-        modload_info.innerHTML = "Windows Mod Loader Download<br>" +
-            "<span id='lin2'>forge-1.7.10-10.13.4.1558-1.7.10-installer-win.exe</span>";
+        if (img_test == null) {
+            var win_img = document.createElement("img");
+            win_img.setAttribute("src", "img/win.png");
+            win_img.setAttribute("id", "modload-img");
+            modload_link.appendChild(win_img);
+            modload_link.setAttribute("href", "downloads/forge/forge-1.7.10-10.13.4.1558-1.7.10-installer-win.exe");
+            modload_info.innerHTML = "Windows Mod Loader Download<br>" +
+                "<span id='lin2'>forge-1.7.10-10.13.4.1558-1.7.10-installer-win.exe</span>";
+        }
     }
 
     if (os == "lin") {
-        var lin_img = document.createElement("img");
-        lin_img.setAttribute("src", "img/lin.png");
-        lin_img.setAttribute("id", "modload-img");
-        modload_link.appendChild(lin_img);
-        modload_link.setAttribute("href", "downloads/forge/forge-1.7.10-10.13.4.1558-1.7.10-universal.jar");
-        modload_info.innerHTML = "Linux Mod Loader Download<br>" +
-            "<span id='lin2'>forge-1.7.10-10.13.4.1558-1.7.10-universal.jar</span>";
+        if (img_test == null) {
+            var lin_img = document.createElement("img");
+            lin_img.setAttribute("src", "img/lin.png");
+            lin_img.setAttribute("id", "modload-img");
+            modload_link.appendChild(lin_img);
+            modload_link.setAttribute("href", "downloads/forge/forge-1.7.10-10.13.4.1558-1.7.10-universal.jar");
+            modload_info.innerHTML = "Linux Mod Loader Download<br>" +
+                "<span id='lin2'>forge-1.7.10-10.13.4.1558-1.7.10-universal.jar</span>";
+        }
     }
 }
 
 function detectOS() {
     var os = navigator.appVersion.substr(5, 3).toLowerCase();
     return os;
+}
+
+
+// fade functions are adapted form SO
+// https://stackoverflow.com/questions/6121203/how-to-do-fade-in-and-fade-out-with-javascript-and-css
+
+function fadeOut(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function fadeIn(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
 }
 
